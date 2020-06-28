eb50fafe17790ddb2579801d743e26a831b446d5
Denis Povazhnyy <d.povazhnyy@povazhnyy.local>
2020-06-28 23:56:08 +0600

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,62 @@
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        dots: false,
-        autoWidth: true,
-        nav: true,
-
-
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 4
+$('.slider-main').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow" style=""><svg class="slick-prev-svg" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 240.823 240.823" xml:space="preserve"><path id="Chevron_Right" d="M57.633,129.007L165.93,237.268c4.752,4.74,12.451,4.74,17.215,0c4.752-4.74,4.752-12.439,0-17.179 l-99.707-99.671l99.695-99.671c4.752-4.74,4.752-12.439,0-17.191c-4.752-4.74-12.463-4.74-17.215,0L57.621,111.816 C52.942,116.507,52.942,124.327,57.633,129.007z"></path></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow" style=""><svg class="slick-next-svg" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 240.823 240.823" xml:space="preserve"><path id="Chevron_Right" d="M57.633,129.007L165.93,237.268c4.752,4.74,12.451,4.74,17.215,0c4.752-4.74,4.752-12.439,0-17.179 l-99.707-99.671l99.695-99.671c4.752-4.74,4.752-12.439,0-17.191c-4.752-4.74-12.463-4.74-17.215,0L57.621,111.816 C52.942,116.507,52.942,124.327,57.633,129.007z"></path></svg></button>',
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         }
-    })
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    verticalSwiping: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true,
+    centerMode: true,
+    vertical: true,
+    verticalSwiping: true,
+    adaptiveHeight: false,
+    prevArrow: '.slick-up',
+    nextArrow: '.slick-down',
+    centerPadding: '100px',
 });
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
@@ -38,26 +76,7 @@ $(document).ready(function() {
         });
     }, false);
 })();
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    verticalSwiping: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    focusOnSelect: true,
-    centerMode: true,
-    vertical: true,
-    verticalSwiping: true,
-    adaptiveHeight: false,
-    prevArrow: '.slick-prev',
-    nextArrow: '.slick-next',
-    centerPadding: '100px',
-});
+
+
+
 $("input[type='number']").inputSpinner()
