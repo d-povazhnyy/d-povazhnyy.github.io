a6b1f069b022bc6674b848d8a469dd0ffc67a6dd
Povaznyy Denis <d.povazhnyy@gmail.com>
2021-04-16 01:26:45 +0300

    Landing_1
    ad Landing_1

---
+++ landing_1/assets/js/main.js
@@ -0,0 +1,36 @@
+$('.reviews__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    variableWidth: false,
+    adaptiveHeight: false,
+    prevArrow: $('.reviews__slider__prev'),
+    nextArrow: $('.reviews__slider__next'),
+    appendDots: $('.reviews__slider__dots'),
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
